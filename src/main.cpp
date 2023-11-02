66ca299e0bd100156ce285dc6545a03f3f55ce61
Alan Ruelas <A01641426@tec.mx>
2023-11-02 09:17:23 -0600

    Creation of weight matrix

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,24 @@
 
 using namespace std;
 
+int** createGraph(int);
+
 int main() {
     
+    int numNeighborhoods;
+    cout << "Number of neighborhoods";
+    cin >> numNeighborhoods;
+
     return 0;
+}
+
+int** createGraph(int n) {
+    int** weightMatrix = new int*[n];
+    for (int i = 0; i < n; i++) {
+        weightMatrix[i] = new int[n];
+        for (int j = 0; j < n; j++) {
+            cin >> weightMatrix[i][j];
+        }
+    }
+    return weightMatrix;
 }
