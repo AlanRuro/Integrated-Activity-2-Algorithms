172c673d4886e8a600a8561c9d8ebb6190702fcb
Gerardo Sanchez <A01641788@tec.mx>
2023-11-24 20:10:50 -0600

    BFS implementation added

--- src/MAX_FLOW/maxFlow.cpp
+++ src/MAX_FLOW/maxFlow.cpp
@@ -18,28 +18,43 @@ T** emptyMatrix(int n, int m) {
     return matrix;
 }
 
-int EdmondsKarp(Graph graph, int s, int t){
-    int** flows = emptyMatrix<int>(graph.getNumVertices(), graph.getNumVertices());
-    Graph residualGraph = Graph(graph.getNumVertices());
+void deleteMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+int EdmondsKarp(Graph graph, int s, int t) {
+    int numVertices = graph.getNumVertices();
+    int** flows = emptyMatrix<int>(graph.getNumVertices(), graph.getNumVertices()); 
+
+    Graph residualGraph(numVertices);
     updateResidualGraph(graph, residualGraph, flows);
     std::vector<int> path = BFS(residualGraph, s, t);
     while (!path.empty()) {
         int residualCapacity = findMinResidualCapacity(path, residualGraph);
-        for (int i = 0; i < path.size()-1; i++) {
+        for (size_t i = 0; i < path.size() - 1; i++) {
             int u = path[i];
-            int v = path[i+1];
+            int v = path[i + 1];
             if (graph.getWeightMatrix()[u][v] != 0) {
-                flows[u][v] = flows[u][v] + residualCapacity;
+                flows[u][v] += residualCapacity;
             } else {
-                flows[v][u] = flows[v][u] - residualCapacity;
+                flows[v][u] -= residualCapacity;
             }
         }
         updateResidualGraph(graph, residualGraph, flows);
         path = BFS(residualGraph, s, t);
     }
 
-    return 0;
+    int maxFlow = 0;
+    for (int i = 0; i < numVertices; ++i) {
+        maxFlow += flows[s][i];
+    }
 
+    deleteMatrix(flows, numVertices);
+
+    return maxFlow;
 }
 
 int findMinResidualCapacity(std::vector<int> path, Graph residualGraph) {
@@ -59,11 +74,51 @@ int findMinResidualCapacity(std::vector<int> path, Graph residualGraph) {
 }
 
 void updateResidualGraph(Graph capacities, Graph& residualGraph, int** flows) {
-    for (int i = 0; i < residualGraph.getNumVertices(); i++) {
-        for (int j = 0; j < residualGraph.getNumVertices(); j++) {
+    int numVertices = capacities.getNumVertices();
+    for (int i = 0; i < numVertices; i++) {
+        for (int j = 0; j < numVertices; j++) {
             int totalResidualCapacity = capacities.getWeightMatrix()[i][j] - flows[i][j] + flows[j][i];
             residualGraph.getWeightMatrix()[i][j] = totalResidualCapacity;
         }
     }
     residualGraph.fillEdges();
 }
+
+std::vector<int> BFS(Graph graph, int s, int t) {
+    int numVertices = graph.getNumVertices();
+    std::vector<int> parent(numVertices, -1);
+    std::queue<int> q;
+    std::vector<bool> visited(numVertices, false);
+
+    q.push(s);
+    visited[s] = true;
+
+    while (!q.empty()) {
+        int current = q.front();
+        q.pop();
+
+        if (current == t) {
+            std::vector<int> path;
+            int currentPath = t;
+            while (currentPath != -1) {
+                path.push_back(currentPath);
+                currentPath = parent[currentPath];
+            }
+            std::reverse(path.begin(), path.end());
+            return path;
+        }
+
+        const auto& weightMatrix = graph.getWeightMatrix();
+        for (int i = 0; i < numVertices; ++i) {
+            if (!visited[i] && weightMatrix[current][i] > 0) {
+                q.push(i);
+                parent[i] = current;
+                visited[i] = true;
+            }
+        }
+    }
+
+    return std::vector<int>();
+}
+
+
--- src/MAX_FLOW/maxFlow.h
+++ src/MAX_FLOW/maxFlow.h
@@ -6,12 +6,14 @@
 #include <vector>
 #include <algorithm>
 #include "../Graph.h"
+#include <queue>
 
 
 int EdmondsKarp(Graph, int, int);
 std::vector<int> BFS(Graph, int, int); // O(V(E*E))
 int findMinResidualCapacity(std::vector<int>, Graph);
 void updateResidualGraph(Graph, Graph&, int**);
+void deleteMatrix(int**, int);
 
 template <class T> T* emptyArray(int);
 template <class T> T** emptyMatrix(int, int);
